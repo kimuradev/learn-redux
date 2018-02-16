a98e1268db64ff035dfb758907b795322427ee39
Leandro Kimura <leandro.kimura@mirumagency.com>
2018-02-16 17:02:06 -0200

    15

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -4,7 +4,21 @@
 //2. copy of current state
 
 function posts(state = [], action) {
-  console.log(state, action);
+
+  switch (action.type) {
+    case 'INCREMENT_LIKES':
+      //return the updated state
+      const i = action.index;
+      return [
+        ...state.slice(0,i), //before the one we are updating
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i + 1) //after the one we are updating
+      ]
+      break;
+    default:
+      return state;
+
+  }
   return state;
 }
 
--- client/store.js
+++ client/store.js
@@ -16,6 +16,6 @@ const defaultState = {
 
 const store = createStore(rootReducer, defaultState);
 
-const history = syncHistoryWithStore(browserHistory, store);
+export const history = syncHistoryWithStore(browserHistory, store);
 
 export default store;
